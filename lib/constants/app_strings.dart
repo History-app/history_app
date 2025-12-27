3fb4a4a0ab7ee7d21c7959e1d4d84fb70b1c8702
Taniguchiyuto <youdougukou5@gmail.com>
2025-12-28 02:36:57 +0900

    AIコメントプロモーションコードを作成

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,18 @@
+class Strings {
+  Strings._() {
+    throw AssertionError('private constructor');
+  }
+
+  // font name
+  static const fontHiraginoKakuGothicPro = 'Hiragino Kaku Gothic Pro';
+
+  // common
+
+  // errors
+
+  // my_page
+  static const aiGeneratedQuestion = 'AIによる問題生成';
+  static const aiQuestionAutoSwitchDescription = '学習状況に応じて、\nAI生成の問題と元の\n問題が切り替わります';
+
+  // others
+}
