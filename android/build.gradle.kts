27c23b3a185017d42adb26065644e7b0c579b1dc
Taniguchiyuto <youdougukou5@gmail.com>
2026-01-02 05:20:31 +0900

    アンドロイド16KB対応

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,16 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -19,6 +29,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
