4631d48a537177a1da4ca65370a97ad41b4a42dd
Taniguchiyuto <youdougukou5@gmail.com>
2026-01-19 04:07:45 +0900

    色と大きさと文字

---
+++ lib/constant/app_size.dart
@@ -0,0 +1,5 @@
+const horizontalPaddingNormal = 20.0;
+const buttonHeight = 56.0;
+const textSizeMedium = 14.0;
+const progressCircleRadius = 16.5;
+const progressCircleDiameter = 33.0;
--- lib/constant/app_strings.dart
+++ lib/constant/app_strings.dart
@@ -5,6 +5,7 @@ class Strings {
 
   // common
   static const title = 'これだけ日本史';
+  static const loginText = 'ログイン';
   static const home = 'ホーム';
   static const data = 'データ';
   static const profile = 'プロフィール';
@@ -17,6 +18,14 @@ class Strings {
   static const confirmDiscardChanges = '変更を破棄しますか？';
   static const continueEditing = '編集を続ける';
   static const memo = 'メモ';
+  static const registerNewAccountLabel = '新規登録';
+  static const mailSendButton = 'メールを送信';
+  static const mailAppOpenButton = 'メールアプリを開く';
+  static const cancel = 'キャンセル';
+  static const delete = '消去する';
+  static const logout = 'ログアウト';
+  static const accountLogoutModalTitle = 'ログアウトしますか？';
+  static const deleteAllData = 'データの全消去';
 
   // errors
   static const failedToOpenUrlMessage = 'URLを開けませんでした';
@@ -54,6 +63,21 @@ class Strings {
 
   static const discardChanges = '破棄';
 
+  static const mailSendedTitle = 'にメールを送信しました！';
+  static const mailSignUpText = '1タップで登録できるURLをメール送信します。';
+  static const mailSignInText = 'ログイン用のURLをメール送信します。';
+  static const mailDeleteText = '確認用のURLをメール送信します。';
+  static const mailInputFormTItle = 'メールアドレス';
+  static const mailAppBarDelete = 'アカウントの削除';
+  static const mailAppBarSended = '送信完了';
+  static const accountDeleteTitle = 'アカウントの削除';
+  static const accountDeletedModalReturnTopButton = 'トップに戻る';
+  static const accountDeletedModalTitle = 'アカウントは消去されました。\nまたのご利用をお待ちしております。';
+  static const accountDeleteWordsDeleteTitle = '今までの学習データは\n完全に消去されます';
+  static const accountDeleteStockDeleteDesc = '一度消去してしまうと、今までの\n学習データは復元できません。';
+  static const accountDeleteSubTitle = 'ご確認をお願いします';
+  static const accountDeleteCheckBoxTitle = '上記を確認しました';
+
   static const dailyNewLearningCards = '一日の新規学習カード';
   static const newCardEra = '新規カードの時代';
   static const eraNotes = '時代別ノート';
@@ -71,6 +95,8 @@ class Strings {
   static const versionUpdateNotice = 'バージョン更新のお知らせ';
   static const updateAvailableMessage = '新しいバージョンのアプリが利用可能です。ストアより更新版を入手して、ご利用下さい。';
   static const updateNowLabel = '今すぐ更新';
+  static const loginSignupMessage = 'アカウントを登録/ログインしましょう';
+  static const accountDeleteModalCheck = '上記を確認しました';
 
   static const questionInstructionMessage = 'ここに表示される問題を確認して、答えを思い出せるか考えてみましょう。';
   static const answerCheck = '解答確認';
@@ -86,6 +112,7 @@ class Strings {
       'このボタンをタップすると、今日学習すべきカードが表示されます。新規カード、学習中のカード、復習カードの順に表示されます。';
   static const dailyLearningMessage = '毎日ログインして学習することが大事\n千里の道も一歩から！';
   static const imageLoadErrorMessage = '通信エラーのため画像を表示できません\nアプリを再起動して、もう一度お試しください。';
+  static const accessibleFromAnyDevice = 'どの端末からでもアクセス可能になります。';
 
   // my_page
 
